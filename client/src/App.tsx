21967d045def9cc43220b54f0cc07c2eca815407
ShowGa <peter1997217123@gmail.com>
2025-03-04 14:55:41 +0800

    <Progress> 1. Build structure of the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout1 from "./layouts/Layout1";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PrivateRoute from "./route-protect/PrivateRoute";
+import PlayingChess from "./components/PlayingChess";
+import PlayingChess2 from "./components/PlayingChess2";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout1 />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/playing-chess-friends" element={<PlayingChess />} />
+            <Route path="/playing-chess-matching" element={<PlayingChess2 />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/PlayingChess2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlayingChess2 = () => {
+  return <div>PlayingChess2</div>;
+};
+
+export default PlayingChess2;
