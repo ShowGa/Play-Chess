ba59551ab1d8f26dd83631262474c5b74978d8b9
ShowGa <peter1997217123@gmail.com>
2025-03-10 23:54:59 +0800

    <Progress> 1. Create GameOverModal.tsx

---
+++ client/src/components/GameOverModal.tsx
@@ -0,0 +1,9 @@
+const gameoverModal = () => {
+  return (
+    <div className="h-[20rem] w-[20rem] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      gameoverModal
+    </div>
+  );
+};
+
+export default gameoverModal;
--- client/src/pages/PlayingChess.tsx
+++ client/src/pages/PlayingChess.tsx
@@ -5,6 +5,8 @@ import { useRef } from "react";
 import socket from "../socket/socket";
 import useAuthStore from "../zustand/useAuthStore";
 
+import GameOverModal from "../components/gameoverModal";
+
 const PlayingChess = () => {
   const {
     // game,
@@ -57,7 +59,7 @@ const PlayingChess = () => {
     <div className="py-10 pl-[15rem] min-h-[100vh] h-1 bg-gray-800">
       <div className="flex gap-4 h-full w-full">
         <div className="flex-shrink flex-grow max-w-[42rem] h-full w-full">
-          <div className="rounded-md overflow-hidden">
+          <div className="rounded-md overflow-hidden relative">
             <Chessboard
               customSquareStyles={customSquareStyles}
               position={fen}
@@ -67,8 +69,12 @@ const PlayingChess = () => {
               boardOrientation={`${you?.color === "w" ? "white" : "black"}`}
             />
             <p className="text-red-700"></p>
+
+            <GameOverModal />
           </div>
         </div>
+
+        {/* right section  */}
         <div className="bg-gray-900 rounded-md flex-grow flex-shrink max-w-[26rem] ">
           <button
             className="text-white border-2 p-2 rounded-md"
